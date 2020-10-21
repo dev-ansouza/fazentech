ab55d5ffa9e99726564d07ac20ae978b021f6254
Arthur Nunes <arthurnx98@gmail.com>
2020-10-21 20:47:59 -0300

    Continuação do trabalho na tela de listagem dos itens

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import { Component } from 'react';
-import { View, FlatList, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
+import { View, FlatList, StyleSheet, Image, Text, TextInput, TouchableOpacity, Button} from 'react-native';
+import NumericInput from 'react-native-numeric-input';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 export default class List extends Component{
+
+  // onSub = (item, index) => {
+  //   this.setState({
+  //     quantity: this.state.quantity - 1 
+  //   })
+  // }
+
+  // onAdd = (item, index) => {
+  // //   this.setState({
+  // //     quantity: this.state.quantity + 1
+  // //   })
+
+  // console.log(item, index);
+  //   const dados = [this.state.dados];
+  //   dados[index].quantity += 1;
+  //   this.setState ({dados});
+  // }
 
   render() {
 
     const dados = [
-      {key: 'Banana', price: 'R$ 1,99 / Kg', image: 'https://feiradoceasa.com.br/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/b/a/banana-nanica-1kg_6_2.jpg'},
-      {key: 'Mamão', price: 'R$ 1,99 / Kg', image: 'https://www.mambo.com.br/ccstore/v1/images/?source=/file/v4925567681571308600/products/131556.jpg&height=400&width=400'},
-      {key: 'Laranja ', price: 'R$ 1,99 / Kg', image: 'https://d26lpennugtm8s.cloudfront.net/stores/746/397/products/laranja_ceu1-53852a66aa8037bd7415221636647098-1024-1024.jpg'},
-      {key: 'Leite     ', price: 'R$ 1,99 / Kg', image: 'https://radiosimpatia.com.br/images/2014/06/leite-emagrece.jpg'},
-      {key: 'Alface   ', price: 'R$ 1,99 / Kg', image: 'https://lh3.googleusercontent.com/proxy/oFqQW_5unSigJWPEOv5mySHdVZLM6M0ZTW32AaPowdmH5L9L3PU7-7vmJtA5txUEUJ3xjpKzB4yaOEWWteoMKqsRjkEYp2J_4df6s9eXnEZ77avS'},
-      {key: 'Tomate ', price: 'R$ 1,99 / Kg', image: 'https://static1.squarespace.com/static/5b8edfa12714e508f756f481/5bb7d929a4222f8b31418ae5/5bbcf80015fcc00ead6882ba/1544815130008/?format=1500w'},
-      {key: 'Manga  ', price: 'R$ 1,99 / Kg', image: 'https://static3.tcdn.com.br/img/img_prod/350075/muda_de_manga_tommy_feita_por_enxerto_5067_2_20190521161350.jpg'},
+      {key: 'Banana', price: 'R$ 1,99 / Kg', quantity: 0, image: 'https://feiradoceasa.com.br/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/b/a/banana-nanica-1kg_6_2.jpg'},
+      {key: 'Mamão', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://www.mambo.com.br/ccstore/v1/images/?source=/file/v4925567681571308600/products/131556.jpg&height=400&width=400'},
+      {key: 'Laranja ', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://d26lpennugtm8s.cloudfront.net/stores/746/397/products/laranja_ceu1-53852a66aa8037bd7415221636647098-1024-1024.jpg'},
+      {key: 'Leite     ', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://radiosimpatia.com.br/images/2014/06/leite-emagrece.jpg'},
+      {key: 'Alface   ', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://lh3.googleusercontent.com/proxy/oFqQW_5unSigJWPEOv5mySHdVZLM6M0ZTW32AaPowdmH5L9L3PU7-7vmJtA5txUEUJ3xjpKzB4yaOEWWteoMKqsRjkEYp2J_4df6s9eXnEZ77avS'},
+      {key: 'Tomate ', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://static1.squarespace.com/static/5b8edfa12714e508f756f481/5bb7d929a4222f8b31418ae5/5bbcf80015fcc00ead6882ba/1544815130008/?format=1500w'},
+      {key: 'Manga  ', price: 'R$ 1,99 / Kg',quantity: 0,  image: 'https://static3.tcdn.com.br/img/img_prod/350075/muda_de_manga_tommy_feita_por_enxerto_5067_2_20190521161350.jpg'},
     ]
 
     return (
       <View style={styles.container}>
 
-          <View style={{ alignItems:'center',justifyContent:'center', marginBottom: 3, borderBottomColor: 'gray', borderBottomWidth: 1}}>
+         <View style={{ alignItems:'center',justifyContent:'center', marginBottom: 3, borderBottomColor: 'gray', borderBottomWidth: 1}}>
             <Text style={{fontSize: 30, margin: 10}}>Selecione a quantidade desejada de cada item:</Text>
           </View>
 
           <FlatList 
               data={dados}
-              renderItem= {( {item}) => 
+              renderItem= {( {item, index}) => 
                 <View style={{flex: 1, flexDirection: 'row', marginBottom: 3, borderBottomColor: 'gray', borderBottomWidth: 1}}>
                   <Image source={{uri: item.image}} style={{width: 80, height: 80, margin: 5}}/>
                   <View style={{flex: 1, flexDirection: 'row', marginBottom: 3, width: '100%'}}>
@@ -34,6 +53,11 @@ export default class List extends Component{
                     </Text>   
                     <Text style={{fontSize: 18, marginTop: 30, marginLeft: 30, color: 'red'}}>{item.price}</Text>
                   </View>
+                  {/* <View style={{flexDirection: 'row', flex: 1, alignItems: 'center', marginLeft: 80}}>
+                    <Button title="Sub" onPress={this.onSub(item, index)}  />
+                    <Text style={{fontSize: 20}}>  {item.quantity}  </Text>
+                    <Button title="Add" onPress={this.onAdd(item, index)} />
+                  </View> */}
                 </View>
               }          
           />
